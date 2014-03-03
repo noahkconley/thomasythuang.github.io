7c514e3bd3d02cff73fec7bdeb74382df44694c9
thomasythuang <thomas.yt.huang@gmail.com>
2014-03-03 17:48:28 -0600

    styling changes

---
+++ business.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    var collapsed = true;
+    $("#discount").click(function(){
+        if (collapsed)
+            $("#chevron").removeClass("glyphicon-chevron-down").addClass("glyphicon-chevron-up");   
+        else
+            $("#chevron").removeClass("glyphicon-chevron-up").addClass("glyphicon-chevron-down");
+
+        collapsed = !collapsed;
+    });
+    
+    $("#set").click(function(){
+        alert("Your reminder has been set!");
+        $("#remind").click();
+    })
+});
